f977ac92749498c2b8c92de4d09bad49ac4362a5
Eduardo <edu.he.dev@gmail.com>
2024-01-26 23:47:12 -0300

    class

---
+++ class.ts
@@ -0,0 +1,31 @@
+class Point {
+  x: number;
+  y: number;
+}
+
+const pt = new Point();
+pt.x = 0;
+pt.y = 0;
+
+
+
+class Thing {
+  _size = 0;
+
+  get size(): number {
+    return this._size;
+  }
+
+  set size(value: string | number | boolean) {
+    let num = Number(value);
+
+    // Don't allow NaN, Infinity, etc
+
+    if (!Number.isFinite(num)) {
+      this._size = 0;
+      return;
+    }
+
+    this._size = num;
+  }
+}
